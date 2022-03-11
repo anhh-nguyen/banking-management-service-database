01049128f5eae749480d1e9364e19c7f8eb1da8e
Anh Nguyen <anhnguyen@lawn-128-61-19-30.lawn.gatech.edu>
2022-03-11 13:05:38 -0500

    add phase 1 and phase 2

---
+++ Phase 2/Requirements/cs4400_phase2_database_template_v0.sql
@@ -0,0 +1,24 @@
+-- CS4400: Introduction to Database Systems (Spring 2022)
+-- Phase II: Create Table & Insert Statements [v0] Monday, February 21, 2022 @ 11:00pm EDT
+
+-- Team __
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+
+-- Directions:
+-- Please follow all instructions for Phase II as listed on Canvas.
+-- Fill in the team number and names and GT usernames for all members above.
+-- Create Table statements must be manually written, NOT taken from an SQL Dump file.
+-- The file must run without error to receive any credit.
+
+-- Recommendations:
+-- You may arrange your create table() and insert() statements in any order.
+-- Develop a "strategy" to determine the order in which you will implement the tables.
+-- Then, implement each table, and ensure that you can load the data correctly before
+-- beginning work on the next table.
+
+-- ------------------------------------------------------
+-- CREATE TABLE STATEMENTS AND INSERT STATEMENTS BELOW
+-- ------------------------------------------------------
